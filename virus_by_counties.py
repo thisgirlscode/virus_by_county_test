ff2aca41c7f5bd57011fb1b4158021af93428222
Patricia-Schultz <patriciaschultz.c@gmail.com>
2020-04-13 18:22:38 -0700

    Include variables inside while loop

--- virus_by_counties.py
+++ virus_by_counties.py
@@ -3,11 +3,12 @@ import numpy as np
 
 my_df = pd.read_csv("test_data.csv")
 
-state = str(input("What state would you like to consult? Example: Washington \n").lower().capitalize())
-county = str(input("Enter county (insert all to remove filter) \n").lower().capitalize())
-
 while True:
     print("Welcome to this exercise")
+    
+    state = str(input("What state would you like to consult? Example: Washington \n").lower().capitalize())
+    county = str(input("Enter county (insert all to remove filter) \n").lower().capitalize())
+
     try:
         if state == "" or county == "":
             print("The values you entered were not valid. Try again")
